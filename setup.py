5906907138d820152d69bdc4b02c6970debf60b9
Nikita Smirnov <46124551+Nikita-Smirnov-Exactpro@users.noreply.github.com>
2023-10-12 16:15:31 +0400

    [TH2-4989] Updated libraries (#13)
    * [TH2-4989] Updated libraries
    * added `createDemoRuleCancelReplace` rpc method

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#   Copyright 2020-2021 Exactpro (Exactpro Systems Limited)
+#   Copyright 2020-2023 Exactpro (Exactpro Systems Limited)
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ class ProtoGenerator(Command):
 
     def run(self):
         proto_path = os.path.abspath('src/main/proto')
-        gen_path = os.path.abspath('src/gen/main/python')
+        gen_path = os.path.abspath('build/generated/source/proto/main/services/python')
 
         if not os.path.exists(gen_path):
             os.makedirs(gen_path)
@@ -58,7 +58,8 @@ class ProtoGenerator(Command):
         for proto_file in proto_files:
             command = ['grpc_tools.protoc'] + \
                       protos_include + \
-                      ['--python_out={}'.format(gen_path), '--grpc_python_out={}'.format(gen_path)] + \
+                      [f'--python_out={gen_path}', f'--grpc_python_out={gen_path}'] + \
+                      [f'--mypy_out={gen_path}'] + \
                       [proto_file]
 
             if protoc.main(command) != 0:
@@ -71,9 +72,9 @@ class CustomDist(sdist):
     def run(self):
         copy_tree(f'src/main/proto/{package_name}', package_name)
 
-        copy_tree(f'src/gen/main/python/{package_name}', package_name)
-        copy_tree(f'src/gen/main/services/python/{package_name}', package_name)
+        copy_tree(f'build/generated/source/proto/main/services/python/{package_name}', package_name)
         Path(f'{package_name}/__init__.py').touch()
+        Path(f'{package_name}/py.typed').touch()
 
         def make_packages(root_dir):
             for path in Path(root_dir).iterdir():
@@ -85,7 +86,8 @@ class CustomDist(sdist):
 
         self.distribution.packages = [''] + find_packages(include=[package_name, f'{package_name}.*'])
         self.distribution.package_data = {'': ['package_info.json'],
-                                          **dict.fromkeys(self.distribution.packages[1:], ['*.proto'])}
+                                          **dict.fromkeys(self.distribution.packages[1:],
+                                                          ['*.proto', 'py.typed', '*.pyi'])}
 
         sdist.run(self)
 
@@ -102,7 +104,8 @@ with open('README.md', 'r') as file:
     long_description = file.read()
 
 packages = [''] + find_packages(include=[package_name, f'{package_name}.*'])
-package_data = {'': ['package_info.json'], **dict.fromkeys(packages[1:], ['*.proto'])}
+package_data = {'': ['package_info.json'],
+                **dict.fromkeys(packages[1:], ['*.proto', 'py.typed', '*.pyi'])}
 
 
 setup(
@@ -117,8 +120,8 @@ setup(
     license='Apache License 2.0',
     python_requires='>=3.7',
     install_requires=[
-        'th2-grpc-sim~=3.1.3',
-        'grpcio-tools==1.38.1'
+        'th2-grpc-sim~=5.1.0rc1',
+        'th2-grpc-common~=4.3.0.dev0'
     ],
     packages=packages,
     package_data=package_data,
